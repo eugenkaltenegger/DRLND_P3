5f257a7409c02073ff05f67c371bed1def26108a
Eugen Kaltenegger <eugen.kaltenegger@gmail.com>
2022-06-23 19:54:18 +0200

    adding more comments to hp and hpr files

--- src/hyperparameters/hyperparameters.py
+++ src/hyperparameters/hyperparameters.py
@@ -19,28 +19,32 @@ class Hyperparameters:
         hp["episodes"] = 10000          # maximum episodes
         hp["steps"] = 1000              # steps per episode
 
+        hp["tau"] = 0.5  # tau (0 < tau < 1)
+        hp["discount"] = 0.9  # discount factor
+
+        # buffer parameters
         hp["buffer_size"] = 100         # replay buffer size
         hp["buffer_frequency"] = 2      # replay buffer learning frequency (number of steps)
         hp["buffer_sample_size"] = 4    # replay buffer sample size
 
+        # action noise parameters
         hp["noise"] = 1.00              # maximum noise (starting noise)
         hp["noise_minimum"] = 0.01      # minimum noise (ending noise)
         hp["noise_reduction"] = 0.99    # noise reduction factor per steps
 
-        hp["tau"] = 0.5                 # tau (0 < tau < 1)
-        hp["discount"] = 0.9            # discount factor
+        # actor parameters
+        hp["actor_layers"] = [128, 64]                          # actor layers
+        hp["actor_activation_function"] = torch.nn.ReLU         # actor activation function
+        hp["actor_output_function"] = torch.nn.Tanh             # actor output function
+        hp["actor_optimizer"] = torch.optim.Adam                # actor optimizer
+        hp["actor_optimizer_learning_rate"] = 0.005             # actor optimizer learning rate
 
-        hp["actor_layers"] = [128, 64]
-        hp["actor_activation_function"] = torch.nn.ReLU
-        hp["actor_output_function"] = torch.nn.Tanh
-        hp["actor_optimizer"] = torch.optim.Adam
-        hp["actor_optimizer_learning_rate"] = 0.005
-
-        hp["critic_layers"] = [128, 64]
-        hp["critic_activation_function"] = torch.nn.ReLU
-        hp["critic_output_function"] = torch.nn.ReLU
-        hp["critic_optimizer"] = torch.optim.Adam
-        hp["critic_optimizer_learning_rate"] = 0.005
+        # critic parameters
+        hp["critic_layers"] = [128, 64]                         # critic layers
+        hp["critic_activation_function"] = torch.nn.ReLU        # critic activation function
+        hp["critic_output_function"] = torch.nn.ReLU            # critic output function
+        hp["critic_optimizer"] = torch.optim.Adam               # critic optimizer
+        hp["critic_optimizer_learning_rate"] = 0.005            # critic optimizer learning rate
 
         self.hp = hp
 
--- src/hyperparameters/hyperparameters_range.py
+++ src/hyperparameters/hyperparameters_range.py
@@ -19,28 +19,32 @@ class HyperparametersRange:
         hpr["episodes"] = [10000]        # maximum episodes
         hpr["steps"] = [1000]            # steps per episode
 
+        hpr["tau"] = [0.5]  # tau (0 < tau < 1)
+        hpr["discount"] = [0.9]  # discount factor
+
+        # buffer parameters
         hpr["buffer_size"] = [100]       # replay buffer size
         hpr["buffer_frequency"] = [2]    # replay buffer learning frequency (number of steps)
         hpr["buffer_sample_size"] = [4]  # replay buffer sample size
 
+        # action noise parameters
         hpr["noise"] = [1.00]            # maximum noise (starting noise)
         hpr["noise_minimum"] = [0.01]    # minimum noise (ending noise)
         hpr["noise_reduction"] = [0.99]  # noise reduction factor per steps
 
-        hpr["tau"] = [0.5]               # tau (0 < tau < 1)
-        hpr["discount"] = [0.9]          # discount factor
+        # actor parameters
+        hpr["actor_layers"] = [[128, 64]]                       # actor layers
+        hpr["actor_activation_function"] = [torch.nn.ReLU]      # actor activation function
+        hpr["actor_output_function"] = [torch.nn.Tanh]          # actor output function
+        hpr["actor_optimizer"] = [torch.optim.Adam]             # actor optimizer
+        hpr["actor_optimizer_learning_rate"] = [0.005]          # actor optimizer learning rate
 
-        hpr["actor_layers"] = [[128, 64]]
-        hpr["actor_activation_function"] = [torch.nn.ReLU]
-        hpr["actor_output_function"] = [torch.nn.Tanh]
-        hpr["actor_optimizer"] = [torch.optim.Adam]
-        hpr["actor_optimizer_learning_rate"] = [0.005]
-
-        hpr["critic_layers"] = [[128, 64]]
-        hpr["critic_activation_function"] = [torch.nn.ReLU]
-        hpr["critic_output_function"] = [torch.nn.ReLU]
-        hpr["critic_optimizer"] = [torch.optim.Adam]
-        hpr["critic_optimizer_learning_rate"] = [0.005]
+        # critic parameters
+        hpr["critic_layers"] = [[128, 64]]                      # critic layers
+        hpr["critic_activation_function"] = [torch.nn.ReLU]     # critic activation function
+        hpr["critic_output_function"] = [torch.nn.ReLU]         # critic output function
+        hpr["critic_optimizer"] = [torch.optim.Adam]            # critic optimizer
+        hpr["critic_optimizer_learning_rate"] = [0.005]         # critic optimizer learning rate
 
         self.hpr = hpr
 
